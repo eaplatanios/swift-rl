b0ba43813e546779da06eee4fafc373bdc15dbb5
Anthony Platanios <e.a.platanios@gmail.com>
2019-04-15 19:09:59 -0400

    Added a couple of documentation strings.

--- Sources/ReinforcementLearning/Distribution.swift
+++ Sources/ReinforcementLearning/Distribution.swift
@@ -3,6 +3,10 @@ import TensorFlow
 public protocol Distribution {
   associatedtype Value
 
+  /// Returns the mode of this distribution. If the distribution has multiple modes, then one of
+  /// them is sampled randomly (and uniformly) and returned.
   func mode(seed: UInt64?) -> Value
+
+  /// Returns a random sample drawn from this distribution.
   func sample(seed: UInt64?) -> Value
 }
